78a0370b1b8b5780548b1de9537ac7dfa0c80f9c
Sahil_Shiakh_5 <sahilshoukatshaikh5@gmail.com>
2026-09-15 20:03:33 +0530

    Fix student face login and registration

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -4,7 +4,6 @@ from src.components.header import header_dashborad
 from src.components.footer import footer_dashboard
 from PIL import Image
 import numpy as np 
-from src.database.db import check_teacher_exist, create_teacher, teacher_login
 from src.pipelines.face_pipeline import predict_attendance, get_face_encodings, train_classifier
 from src.pipelines.voice_pipeline import get_voice_embedding
 from src.database.db import get_all_students, create_student
@@ -18,7 +17,7 @@ def student_screen():
     style_background_dashboard()
     style_base_layout()
 
-    if "studen_data" in st.session_state:
+    if "student_data" in st.session_state:
          student_dashboard()
          return
 
@@ -55,7 +54,7 @@ def student_screen():
                 if detected:
                        student_id = list(detected.keys())[0]
                        all_students = get_all_students()
-                       student = next((s for s in all_students["student_id"] == student_id), None)
+                       student = next((s for s in all_students if s["student_id"] == student_id), None)
 
                        if student: 
                             st.session_state.is_logged_in = True
@@ -74,7 +73,7 @@ def student_screen():
             new_name = st.text_input("Enter your name", placeholder="E.g. Elon Musk")
 
             st.subheader("Optional : Voice Enrollment")
-            st.info("Enroll your for voice only attendance")
+            st.info("Enroll your voice for voice-only attendance")
 
             audio_data = None
 
@@ -86,10 +85,10 @@ def student_screen():
             if st.button ("Create Account", type="primary"): 
                  if new_name: 
                       with st.spinner("Creating your profile..."):
-                           img = np.array[Image.open(photo_source)]
+                           img = np.array(Image.open(photo_source))
                            encodings = get_face_encodings(img)
                            if encodings: 
-                                face_emb = encodings[0].tolists()
+                                face_emb = encodings[0].tolist()
 
                                 voice_emb = None
                                 if audio_data: 
@@ -106,7 +105,7 @@ def student_screen():
                                      time.sleep(1)
                                      st.rerun()
                            else:
-                                st.error("Couldnt capture your facial fatures for registration")
+                                st.error("Couldn't capture your facial features for registration")
 
 
                  else: 
