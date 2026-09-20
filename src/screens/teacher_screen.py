9ab4834a1006a918c8990ebfeed42ba909cfe19a
Sahil_Shiakh_5 <sahilshoukatshaikh5@gmail.com>
2026-09-20 13:59:54 +0530

    Add camera function and photo upload tab have been created within the teacher attendance function.

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -6,6 +6,7 @@ from src.components.dialog_create_subject import create_subject_dialog
 from src.database.db import check_teacher_exist, create_teacher, teacher_login, get_teacher_subjects
 from src.components.subject_card import subject_card
 from src.components.dialog_share_subject import share_subject_dialog
+from src.components.dialog_add_photo import add_photos_dialog
 
 
 def teacher_screen():
@@ -73,7 +74,39 @@ def teacher_dashboard():
     footer_dashboard()
 
 def teacher_tab_take_attendance():
+    teacher_id = st.session_state.teacher_data["teacher_id"]
     st.header("Take AI Attendance")
+
+    if "attendance_images" not in st.session_state:
+        st.session_state.attendance_images = []
+
+    subjects = get_teacher_subjects(teacher_id)
+
+    if not subjects: 
+        st.warning("You haven't created any subjects yet! Please create a ine to begin!")
+        return
+
+    subject_options = {f"{s["name"]} - {s["subject_code"]}": s["subject_id"] for s in subjects}
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        selected_subject_label = st.selectbox("Select Subject", options=list(subject_options.keys()))
+
+    with col2: 
+        if st.button("Add Photos", type="primary", icon=":material/photo_prints:", width="stretch"):
+            add_photos_dialog()
+
+
+    selected_subject_id = subject_options[selected_subject_label]
+
+    st.divider()
+
+
+
+
+
+
 
 def teacher_tab_manage_subjects():
     teacher_id = st.session_state.teacher_data["teacher_id"]
